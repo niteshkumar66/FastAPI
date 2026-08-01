4ce05f2b424fe1499a4b7fb09b25b5360c113b2f
niteshkumar66 <nitesh0221kumar@gmail.com>
2026-08-01 15:51:56 +0530

    main

--- main.py
+++ main.py
@@ -20,3 +20,29 @@ def readRoot() :
 @app.get("/teas")
 def get_teas ():
     return teas
+
+
+@app.post("/teas")
+def addTea(tea: Tea):
+    teas.append(tea)
+    return tea
+
+
+@app.put("/teas/{tea_id}")
+def updateTea(tea_id: int, updatedTea : Tea):
+    for index, tea in enumerate(teas):
+        if tea.id == tea_id:
+            teas[index] =updatedTea
+            return updatedTea
+
+    return {"error" : "Tea not found"}
+
+
+@app.delete("/teas/{tea_id}")
+def deleteTea(tea_id: int):
+    for index, tea in enumerate(teas):
+        if tea.id == tea_id:
+            deleted = teas.pop(index)
+            return deleted
+
+    return {"error" : "Tea not found"}
